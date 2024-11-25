8b41dc5ca7a244e5d9c7d7c9d4d2178d8a1afd49
RomaSamkov <Romansamkov.rs@gmail.com>
2024-11-25 12:58:44 +0200

    Update PostsList.tsx

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,10 +1,23 @@
-import { useGetPostsQuery } from "../state/posts/postsAPISlice";
+import {
+  useCreatePostMutation,
+  useGetPostsQuery,
+} from "../state/posts/postsAPISlice";
 
 const PostsList = () => {
   const { data: posts } = useGetPostsQuery({ limit: 5, offset: 0 });
 
+  const [createPostMutation, { isLoading }] = useCreatePostMutation();
+
   return (
     <div>
+      <button
+        onClick={() => {
+          const post = { title: "My post" };
+          createPostMutation(post);
+        }}
+      >
+        {isLoading ? "Loading..." : "Create My Post"}
+      </button>
       <ul>
         {posts?.map((post) => {
           return <li key={post.id}>{post.title}</li>;
