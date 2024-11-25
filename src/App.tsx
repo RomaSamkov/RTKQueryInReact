7a06ec1ac20318caddd22289b1b5243cf07a75a8
RomaSamkov <Romansamkov.rs@gmail.com>
2024-11-25 13:35:54 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import Images from "./components/Images";
 import PostsList from "./components/PostsList";
 
 function App() {
   return (
     <div>
+      <Images />
       <Counter />
       <PostsList />
     </div>
